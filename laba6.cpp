535cfcb735ccc290c90e96fe02ae864dfe17ddae
Supet1337 <58482565+Supet1337@users.noreply.github.com>
2022-12-12 18:34:29 +0300

    Update laba6.cpp

--- laba6.cpp
+++ laba6.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 void is_valid_email(const string& str) {
-    static const regex r(R"(([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+))");
+    static const regex r(R"(([<a href=](https?\:\/\/)?([a-zA-Z0-9]+([a-zA-Z0-9-]+[a-zA-Z0-9]\.[a-zA-Z0-9]+)+(\/?[?=&\d.]?[\w-]+)+)))");
     smatch match;
     if(regex_search(str, match, r))
         cout << match[0] << endl;
@@ -23,9 +23,9 @@ int main()
     std::ifstream fin;
     fin.open("../text.html");
     if (!fin)
-        std::cout << "Ôàéë íå îòêðûò\n\n";
+        std::cout << "Ã”Ã Ã©Ã« Ã­Ã¥ Ã®Ã²ÃªÃ°Ã»Ã²\n\n";
     else
-        std::cout << "Ôàéë îòêðûò!\n\n";
+        std::cout << "Ã”Ã Ã©Ã« Ã®Ã²ÃªÃ°Ã»Ã²!\n\n";
 
     string buf;
     while(getline(fin, buf)){
